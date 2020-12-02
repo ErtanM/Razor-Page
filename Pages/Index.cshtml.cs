f87daa26dcdc3a7f6ff6657e819f3c419a351869
Ertan <ertansofia@gmail.com>
2020-12-02 17:20:20 +0200

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorCW.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public string FirstName { get; set; }
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(FirstName))
+            {
+                FirstName = "Ertan";
+            }
+
+        }
+    }
+}
